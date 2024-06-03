52641d79f6920d71ad1680491d6a16f4dcb33932
saikumarmv <81808040+saikumarmv@users.noreply.github.com>
2024-06-03 22:25:39 +0530

    Create second.groovy

---
+++ jobs/secondgroovy/second.groovy
@@ -0,0 +1,24 @@
+pipelinejob('second pipeline') {
+    agent any
+    options {
+        skipStagesAfterUnstable()
+    }
+    stages {
+        stage('Build') {
+            steps {
+                sh 'make'
+            }
+        }
+        stage('Test'){
+            steps {
+                sh 'make check'
+                junit 'reports/**/*.xml'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                sh 'make publish'
+            }
+        }
+    }
+}
