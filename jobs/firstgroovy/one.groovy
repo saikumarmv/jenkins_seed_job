a457d02af65b6d2eb699489dd98b50e7a7dd071c
saikumarmv <81808040+saikumarmv@users.noreply.github.com>
2024-06-03 22:24:59 +0530

    Create one.groovy

---
+++ jobs/firstgroovy/one.groovy
@@ -0,0 +1,24 @@
+pipelinejob('firstpipiline') {
+    agent any
+    options {
+        skipStagesAfterUnstable()
+    }
+    stages {
+        stage('Build') {
+            steps {
+                sh 'make'
+            }
+        }
+        stage('Test'){
+            steps {
+                sh 'make check'
+                junit 'reports/**/*.xml'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                sh 'make publish'
+            }
+        }
+    }
+}
